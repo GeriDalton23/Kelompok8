4b713b802b7b01c1b93e05d294882f16b90a9321
Fiqri Herdiansyah <geriganteng2302@gmail.com>
2026-04-09 13:24:03 +0700

    menambahkan folder layouts app.blade.php dan menu user

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,5 +3,8 @@
         <li class="mb-3">
             <a href="/" class="block p-2 hover:bg-gray-700 rounded">Dashboard</a>
         </li>
+        <li class="mb-3">
+            <a href="/users" class="block p-2 hover:bg-gray-700 rounded">Users</a>
+        </li>
     </ul>
 </aside>
